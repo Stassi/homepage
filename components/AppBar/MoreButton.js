8786d88e9ecbd43f5a35f77004a076ecc6b9d4a8
Andreas Stassivik <andreas@stassi.net>
2019-12-06 13:57:05 -0800

    transfer props to child

--- components/AppBar/MoreButton.js
+++ components/AppBar/MoreButton.js
@@ -2,8 +2,12 @@ import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import MoreIcon from '@material-ui/icons/MoreVert'
 
-const MoreButton = () => (
-  <IconButton aria-label='display more actions' edge='end'>
+const MoreButton = props => (
+  <IconButton
+    aria-label='display more actions'
+    edge='end'
+    {...props}
+  >
     <MoreIcon />
   </IconButton>
 )
