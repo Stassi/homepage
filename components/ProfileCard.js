f3cd983e9b947d6aae0e929cd27bcb1514c3c20a
Andreas Stassivik <andreas@stassi.net>
2019-12-08 22:50:20 -0800

    profile card

---
+++ components/ProfileCard.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 365
+  },
+  media: {
+    height: 0,
+    paddingTop: `${381 / 365 * 100}%`
+  }
+})
+
+const ProfileCard = () => {
+  const {
+    card: cardClass,
+    media: mediaClass
+  } = useStyles()
+
+  return (
+    <Card className={cardClass}>
+      <CardActionArea>
+        <CardMedia className={mediaClass} image='/stassi-ticket.jpg' />
+
+        <CardContent>
+          <Typography variant='h5'>
+            Andreas Stassivik
+          </Typography>
+
+          <Typography color='textSecondary' variant='subtitle1'>
+            Software Developer
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+
+      <CardActions>
+        <Button color='secondary'>
+          View profile
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default ProfileCard
