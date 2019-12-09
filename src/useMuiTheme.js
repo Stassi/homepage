3e247cb83b8b783b613967983b6872bc88f22d86
Andreas Stassivik <andreas@stassi.net>
2019-12-09 00:30:01 -0800

    toggle dark|light theme

--- src/useMuiTheme.js
+++ src/useMuiTheme.js
@@ -5,14 +5,26 @@ import React, {
 } from 'react'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import DarkenIcon from '@material-ui/icons/Brightness4'
+import LightenIcon from '@material-ui/icons/Brightness7'
 
 const useMuiTheme = initialState => {
-  // TODO: Set theme intent
-  const [intent] = useState(initialState)
+  const [intent, setIntent] = useState(initialState)
 
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) jssStyles.parentElement.removeChild(jssStyles)
+  }, [])
+
+  const handleToggleDarkLight = useMemo(() => (ev) => {
+    ev.preventDefault()
+
+    setIntent(({ palette }) => ({
+      palette: {
+        ...palette,
+        type: palette.type === 'light' ? 'dark' : 'light'
+      }
+    }))
   }, [])
 
   const theme = useMemo(() => createMuiTheme(intent), [intent])
@@ -24,7 +36,13 @@ const useMuiTheme = initialState => {
     </ThemeProvider>
   ), [theme])
 
-  return { Provider, theme }
+  return {
+    DarkenIcon,
+    handleToggleDarkLight,
+    LightenIcon,
+    Provider,
+    theme
+  }
 }
 
 export default useMuiTheme
