6ecacba352935473db58ea4f60084fdcd594a531
Andreas Stassivik <andreas@stassi.net>
2019-12-09 04:38:56 -0800

    implement ToggleDarkLightButton

--- components/AppBar/AppBar.js
+++ components/AppBar/AppBar.js
@@ -1,29 +1,17 @@
 import React from 'react'
 import MuiAppBar from '@material-ui/core/AppBar'
-import IconButton from '@material-ui/core/IconButton'
 import Toolbar from '@material-ui/core/Toolbar'
 import MenuButton from './MenuButton'
 import Title from './Title'
 
-const AppBar = ({
-  handleToggleDarkLight,
-  DarkenIcon,
-  LightenIcon,
-  theme: {
-    palette: {
-      type: themePaletteType
-    }
-  }
-}) => (
+const AppBar = ({ ToggleDarkLightButton }) => (
   <MuiAppBar color='default' position='static'>
     <Toolbar>
       <MenuButton />
 
       <Title {...{ text: '' }} />
 
-      <IconButton color='inherit' onClick={handleToggleDarkLight}>
-        {themePaletteType === 'light' ? <DarkenIcon /> : <LightenIcon />}
-      </IconButton>
+      <ToggleDarkLightButton />
     </Toolbar>
   </MuiAppBar>
 )
