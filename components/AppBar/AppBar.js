cd581bc1e72351c798e376bb7cbcf6160f69e03a
Andreas Stassivik <andreas@stassi.net>
2019-12-09 00:31:32 -0800

    implement dark|light theme

--- components/AppBar/AppBar.js
+++ components/AppBar/AppBar.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import MuiAppBar from '@material-ui/core/AppBar'
+import IconButton from '@material-ui/core/IconButton'
 import Toolbar from '@material-ui/core/Toolbar'
 import MenuButton from './MenuButton'
-import MoreButton from './MoreButton'
 import Title from './Title'
 
-const AppBar = () => (
+const AppBar = ({
+  handleToggleDarkLight,
+  DarkenIcon,
+  LightenIcon,
+  theme: {
+    palette: {
+      type: themePaletteType
+    }
+  }
+}) => (
   <MuiAppBar color='default' position='static'>
     <Toolbar>
       <MenuButton />
 
       <Title {...{ text: '' }} />
 
-      <MoreButton />
+      <IconButton color='inherit' onClick={handleToggleDarkLight}>
+        {themePaletteType === 'light' ? <DarkenIcon /> : <LightenIcon />}
+      </IconButton>
     </Toolbar>
   </MuiAppBar>
 )
