d1adcd493557a162cb181bd9f39046223e81b1c6
Andreas Stassivik <andreas@stassi.net>
2019-12-09 19:24:04 -0800

    slide in AppBar

--- components/AppBar/AppBar.js
+++ components/AppBar/AppBar.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import MuiAppBar from '@material-ui/core/AppBar'
+import Slide from '@material-ui/core/Slide'
 import Toolbar from '@material-ui/core/Toolbar'
 import MenuButton from './MenuButton'
 import Title from './Title'
 
 const AppBar = ({ ToggleDarkLightButton }) => (
-  <MuiAppBar color='default' position='static'>
-    <Toolbar>
-      <MenuButton />
+  <Slide in>
+    <MuiAppBar color='default' position='static'>
+      <Toolbar>
+        <MenuButton />
 
-      <Title {...{ text: '' }} />
+        <Title {...{ text: '' }} />
 
-      <ToggleDarkLightButton />
-    </Toolbar>
-  </MuiAppBar>
+        <ToggleDarkLightButton />
+      </Toolbar>
+    </MuiAppBar>
+  </Slide>
 )
 
 export default AppBar
