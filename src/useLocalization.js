284c61a11b029a1a04c0d9f0242878d7c0449b17
Andreas Stassivik <andreas@stassi.net>
2019-12-10 00:32:42 -0800

    localization hook

---
+++ src/useLocalization.js
@@ -0,0 +1,18 @@
+import { useMemo, useState } from 'react'
+import languages from './localization/languages'
+
+const useLocalization = initialState => {
+  // TODO: Set intent
+  const [languageIntent] = useState(initialState)
+
+  const language = useMemo(() => languages[languageIntent], [languageIntent])
+
+  const Phrase = useMemo(
+    () => ({ children }) => language[children] || children,
+    [language]
+  )
+
+  return { Phrase }
+}
+
+export default useLocalization
