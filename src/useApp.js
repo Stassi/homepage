c7c87af2f94a3558a3182c2b0fb28f7e940e6a5b
Andreas Stassivik <andreas@stassi.net>
2019-12-10 20:20:03 -0800

    implement global language config

--- src/useApp.js
+++ src/useApp.js
@@ -1,14 +1,16 @@
 import {
-  language,
+  languageDefault,
+  languages,
   muiTheme,
   name as titleText
 } from '../package'
-import useLocalization from './localization/useLocalization'
+import useLocalization from './useLocalization'
 import useMuiTheme from './useMuiTheme'
 
 const useApp = () => ({
+  languages,
   titleText,
-  localization: useLocalization(language),
+  localization: useLocalization(languageDefault),
   theme: useMuiTheme(muiTheme)
 })
 
--- src/useLocalization.js
+++ src/useLocalization.js
@@ -1,11 +1,11 @@
 import { useMemo, useState } from 'react'
-import languages from './languages'
+import vocabularies from './localization/vocabularies'
 
 const useLocalization = initialState => {
   // TODO: Set intent
   const [languageIntent] = useState(initialState)
 
-  const language = useMemo(() => languages[languageIntent], [languageIntent])
+  const language = useMemo(() => vocabularies[languageIntent], [languageIntent])
 
   const Phrase = useMemo(
     () => ({ children }) => language[children] || children,
