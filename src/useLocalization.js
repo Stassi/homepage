96e2880795dd522848a7575786fe6519e369426b
Andreas Stassivik <andreas@stassi.net>
2019-12-10 20:32:46 -0800

    consistency

--- src/useLocalization.js
+++ src/useLocalization.js
@@ -5,11 +5,11 @@ const useLocalization = initialState => {
   // TODO: Set intent
   const [languageIntent] = useState(initialState)
 
-  const language = useMemo(() => vocabularies[languageIntent], [languageIntent])
+  const vocabulary = useMemo(() => vocabularies[languageIntent], [languageIntent])
 
   const Phrase = useMemo(
-    () => ({ children }) => language[children] || children,
-    [language]
+    () => ({ children: phrase }) => vocabulary[phrase] || phrase,
+    [vocabulary]
   )
 
   return { Phrase }
