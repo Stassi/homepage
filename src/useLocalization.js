b392d1b7be9609caf9c55dcb88526a24cb220007
Andreas Stassivik <andreas@stassi.net>
2019-12-10 20:35:44 -0800

    return current language

--- src/useLocalization.js
+++ src/useLocalization.js
@@ -2,17 +2,17 @@ import { useMemo, useState } from 'react'
 import vocabularies from './localization/vocabularies'
 
 const useLocalization = initialState => {
-  // TODO: Set intent
-  const [languageIntent] = useState(initialState)
+  // TODO: Set language
+  const [language] = useState(initialState)
 
-  const vocabulary = useMemo(() => vocabularies[languageIntent], [languageIntent])
+  const vocabulary = useMemo(() => vocabularies[language], [language])
 
   const Phrase = useMemo(
     () => ({ children: phrase }) => vocabulary[phrase] || phrase,
     [vocabulary]
   )
 
-  return { Phrase }
+  return { language, Phrase }
 }
 
 export default useLocalization
