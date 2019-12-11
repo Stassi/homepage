017c0455859bc0536523fb01e7e548846624710f
Andreas Stassivik <andreas@stassi.net>
2019-12-10 23:01:20 -0800

    Turkish vocabulary

--- src/localization/vocabularies.js
+++ src/localization/vocabularies.js
@@ -9,6 +9,7 @@ import rus from './rus'
 import spa from './spa'
 import swa from './swa'
 import swe from './swe'
+import tur from './tur'
 import zhos from './zhos'
 import zhot from './zhot'
 
@@ -24,6 +25,7 @@ const vocabularies = {
   spa,
   swa,
   swe,
+  tur,
   zhos,
   zhot
 }
