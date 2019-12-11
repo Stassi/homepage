1e6326d843708b6d840e0d51bcf40182233c8c55
Andreas Stassivik <andreas@stassi.net>
2019-12-10 20:19:07 -0800

    semantic renaming

--- src/localization/languages.js
+++
@@ -1,6 +0,0 @@
-import english from './english'
-import norwegian from './norwegian'
-
-const languages = { english, norwegian }
-
-export default languages
---
+++ src/localization/vocabularies.js
@@ -0,0 +1,6 @@
+import eng from './eng'
+import nor from './nor'
+
+const vocabularies = { eng, nor }
+
+export default vocabularies
