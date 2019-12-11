a05a599606ce238cf6599e724c945097d53cd5f2
Andreas Stassivik <andreas@stassi.net>
2019-12-10 21:43:43 -0800

    Icelandic vocabulary

--- src/localization/vocabularies.js
+++ src/localization/vocabularies.js
@@ -1,10 +1,12 @@
 import eng from './eng'
+import isl from './isl'
 import kor from './kor'
 import nor from './nor'
 import swe from './swe'
 
 const vocabularies = {
   eng,
+  isl,
   kor,
   nor,
   swe
