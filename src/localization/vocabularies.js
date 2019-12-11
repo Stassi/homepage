48b4e24c0f22bcdbfa447d9c0abbdadf7312bffe
Andreas Stassivik <andreas@stassi.net>
2019-12-10 21:34:08 -0800

    Swedish vocabulary

--- src/localization/vocabularies.js
+++ src/localization/vocabularies.js
@@ -1,6 +1,11 @@
 import eng from './eng'
 import nor from './nor'
+import swe from './swe'
 
-const vocabularies = { eng, nor }
+const vocabularies = {
+  eng,
+  nor,
+  swe
+}
 
 export default vocabularies
