af1eb8ee1d808f4d748a8111107c5f3a2c55e6a6
Andreas Stassivik <andreas@stassi.net>
2019-12-16 16:13:48 -0800

    profile actions

---
+++ components/ProfileActions.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import MdiIcon from './MdiIcon'
+
+const useStyles = makeStyles({
+  maxHeight: {
+    height: '100%'
+  }
+})
+
+const ProfileActions = ({ router }) => {
+  const {
+    maxHeight: maxHeightClass
+  } = useStyles()
+
+  return (
+    <Grid
+      alignItems='stretch'
+      container
+      item
+      spacing={1}
+    >
+      <Grid item xs>
+        <Button
+          color='primary'
+          fullWidth
+          onClick={ev => {
+            ev.preventDefault()
+            window.location = 'https://github.com/Stassi'
+          }}
+          startIcon={<MdiIcon name='github' />}
+          variant='contained'
+        >
+          View GitHub profile
+        </Button>
+      </Grid>
+
+      <Grid item xs>
+        <Button
+          className={maxHeightClass}
+          color='default'
+          fullWidth
+          onClick={ev => {
+            ev.preventDefault()
+            // TODO: Replace file with static route
+            router.push('/andreas-stassivik-resume.pdf')
+          }}
+          startIcon={<MdiIcon name='pdf' />}
+          variant='outlined'
+        >
+          View résumé
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default ProfileActions
