8740c42321089925a2bf2e51033ad71e8f33027b
Andreas Stassivik <andreas@stassi.net>
2019-12-18 03:04:39 -0800

    append footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,53 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import Fade from '@material-ui/core/Fade'
+import Slide from '@material-ui/core/Slide'
 import AppBar from './AppBar'
-import Content from './Content'
+import Footer from './Footer'
+import ProfileCard from './ProfileCard'
 
-const Layout = props => (
-  <>
-    <AppBar {...props} />
-    <Content {...props} />
-  </>
-)
+const useStyles = makeStyles(({ spacing }) => ({
+  cardContainer: {
+    paddingBottom: spacing(8),
+    paddingTop: spacing(2)
+  },
+  grow: {
+    flexGrow: 1
+  },
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  }
+}))
+
+const Layout = props => {
+  const {
+    cardContainer: cardContainerClass,
+    grow: growClass,
+    root: rootClass
+  } = useStyles()
+
+  return (
+    <div className={rootClass}>
+      <Fade in>
+        <div>
+          <AppBar {...props} />
+
+          <Container className={cardContainerClass}>
+            <ProfileCard {...props} />
+          </Container>
+        </div>
+      </Fade>
+
+      <div className={growClass} />
+
+      <Slide direction='up' in>
+        <Footer />
+      </Slide>
+    </div>
+  )
+}
 
 export default Layout
