b05331f70e47e8a7b5c50a90910d046349cba9e9
Andreas Stassivik <andreas@stassi.net>
2019-12-22 02:01:46 -0800

    implement ngrams distribution hook

--- components/BlackwellBioethicsGenerator.js
+++ components/BlackwellBioethicsGenerator.js
@@ -3,43 +3,38 @@ import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Fade from '@material-ui/core/Fade'
 import Slide from '@material-ui/core/Slide'
-import { ngramsDistribution } from 'markovian-nlp'
 import Footer from './Footer'
 import ProfileAppBar from './ProfileAppBar'
 import SentenceGeneratorCard from './SentenceGeneratorCard'
-import blackwellConscience from '../src/corpora/blackwellConscience'
-
-const distribution = ngramsDistribution(blackwellConscience)
+import useNgramsDistribution from '../src/useNgramsDistribution'
 
 const useStyles = makeStyles(({ spacing }) => ({
   container: {
     paddingBottom: '100%',
     paddingTop: spacing(2)
-  },
-  root: {}
+  }
 }))
 
 const BlackwellBioethicsGenerator = props => {
   const {
-    container: containerClass,
-    root: rootClass
+    container: containerClass
   } = useStyles()
+
+  const distribution = useNgramsDistribution('blackwellConscience')
 
   return (
     <>
       <ProfileAppBar {...props} />
 
-      <div className={rootClass}>
-        <Fade in>
-          <Container className={containerClass}>
-            <SentenceGeneratorCard {...{ distribution }} />
-          </Container>
-        </Fade>
+      <Fade in>
+        <Container className={containerClass}>
+          <SentenceGeneratorCard {...{ distribution }} />
+        </Container>
+      </Fade>
 
-        <Slide direction='up' in>
-          <Footer />
-        </Slide>
-      </div>
+      <Slide direction='up' in>
+        <Footer />
+      </Slide>
     </>
   )
 }
--- components/LockeEpistleGenerator.js
+++ components/LockeEpistleGenerator.js
@@ -3,43 +3,38 @@ import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Fade from '@material-ui/core/Fade'
 import Slide from '@material-ui/core/Slide'
-import { ngramsDistribution } from 'markovian-nlp'
 import Footer from './Footer'
 import ProfileAppBar from './ProfileAppBar'
 import SentenceGeneratorCard from './SentenceGeneratorCard'
-import lockesEpistle from '../src/corpora/lockesEpistle'
-
-const distribution = ngramsDistribution(lockesEpistle)
+import useNgramsDistribution from '../src/useNgramsDistribution'
 
 const useStyles = makeStyles(({ spacing }) => ({
   container: {
     paddingBottom: '100%',
     paddingTop: spacing(2)
-  },
-  root: {}
+  }
 }))
 
 const LockeEpistleGenerator = props => {
   const {
-    container: containerClass,
-    root: rootClass
+    container: containerClass
   } = useStyles()
+
+  const distribution = useNgramsDistribution('lockesEpistle')
 
   return (
     <>
       <ProfileAppBar {...props} />
 
-      <div className={rootClass}>
-        <Fade in>
-          <Container className={containerClass}>
-            <SentenceGeneratorCard {...{ distribution }} />
-          </Container>
-        </Fade>
+      <Fade in>
+        <Container className={containerClass}>
+          <SentenceGeneratorCard {...{ distribution }} />
+        </Container>
+      </Fade>
 
-        <Slide direction='up' in>
-          <Footer />
-        </Slide>
-      </div>
+      <Slide direction='up' in>
+        <Footer />
+      </Slide>
     </>
   )
 }
